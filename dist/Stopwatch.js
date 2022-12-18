4e218890cae756d9e9032be437f8339d14bd2b27
electricessence <5899455+electricessence@users.noreply.github.com>
2022-12-18 11:16:39 -0800

    Refresh.

--- dist/Stopwatch.js
+++ dist/Stopwatch.js
@@ -5,7 +5,7 @@ const tslib_1 = require("tslib");
  * @author electricessence / https://github.com/electricessence/
  * @license MIT
  */
-const TimeSpan_1 = (0, tslib_1.__importDefault)(require("@tsdotnet/date-time/dist/TimeSpan"));
+const TimeSpan_1 = tslib_1.__importDefault(require("@tsdotnet/date-time/dist/TimeSpan"));
 class Stopwatch {
     constructor() {
         this._startTimeStamp = NaN;
